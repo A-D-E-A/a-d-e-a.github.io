0fbf14df8f6787dd665762e1184320f46384518d
Adam Ambrosino <adam.d.e.ambrosino@gmail.com>
2024-04-04 11:22:53 +0200

    Added caching

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
+const cacheName = "myapp-v1";
+const filesToCache = [
+    "/index.html",
+    "/style.css",
+    "/icons/github-icon_72x72.png",
+    "/icons/github-icon_512x512.png"
+];
 
 self.addEventListener("insall", (e) => {
-    console.log("Service worker install");
+    console.log("[SW] install");
+    e.waitUntil((async () => {
+        const cache = await caches.open(cacheName);
+        console.log("[SW] Caching files");
+        await cache.addAll(filesToCache);
+    })());
+});
+
+
+self.addEventListener("fetch", (e) => {
+    console.log("[SW] Fetching url: ", e.request.url);
+    e.respondWith((async () => {
+        // If the request has already been cached,
+        // return the cached value to avoid unnecessary
+        // network usage.
+        const match = await caches.match(e.request);
+        if (match) return match;
+
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log("[SW] Caching new resource: ", e.request.url);
+        cache.put(e.request, response.clone());
+        return response;
+    })())
 })
-
-/**
- * @param {FetchEvent} e 
- */
-function onFetch(e) {
-    console.log("Requested url: ", e.request.url);
-    e.respondWith(
-        Promise.resolve("No, you cannot request anything!")
-    )
-}
-
-self.addEventListener("fetch", onFetch)
